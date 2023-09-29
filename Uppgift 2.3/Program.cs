47a2014c7bb9dfd2e0d2c382b562f56111f3fce0
Admin <Admin@TEB123018>
2023-09-29 14:51:38 +0200

    Add project files.

---
+++ Uppgift 2.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Uppgift2_3
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            
+            int startavgift = 300;
+            int kostnadPerKilometer = 1;
+            int extraDagKostnad = 500;
+
+            
+            Console.Write("Hur många dagar vill du hyra bilen: ");
+            int antalDagar = int.Parse(Console.ReadLine());
+
+            Console.Write("Hur många kilometer ska du köra: ");
+            int antalKilometer = int.Parse(Console.ReadLine());
+
+           
+            int totalHyra = startavgift + (antalDagar - 1) * extraDagKostnad + antalKilometer * kostnadPerKilometer;
+
+            
+            Console.WriteLine($"Den totala hyran blir: {totalHyra} kr");
+
+            
+            Console.ReadLine();
+        }
+    }
+}
